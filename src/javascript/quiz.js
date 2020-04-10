067b3b5f20ac9308e9235659a01417aafea57dc3
David Twomey <davidmatt5152@gmail.com>
2020-04-10 20:41:13 +0100

    saving

--- src/javascript/quiz.js
+++ src/javascript/quiz.js
@@ -11,3 +11,49 @@ buildQuiz();
 
 // on submit, show results
 submitButton.addEventListener('click', showResults);
+
+const myQuestions = [
+    {
+        question: "What does UX stand for?",
+        answers: {
+            a: "User expectations",
+            b: "User evaluation",
+            c: "User experience"
+        },
+        correctAnswer: "C"
+    },
+    {
+        question: "How many participants should suffice for a discount usability test?",
+        answers: {
+            a: "6",
+            b: "5",
+            c: "7"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Most important part of design is how it looks",
+        answers: {
+            a: "True",
+            b: "False",
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "When performing a usability test, you should:",
+        answers: {
+            a: "",
+            b: "5",
+            c: "7"
+        },
+        correctAnswer: "b"
+    },
+
+
+
+
+
+
+
+
+]
